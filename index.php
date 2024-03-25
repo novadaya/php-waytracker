f18c10c83691c843f35b9779e5b782073044b7eb
Norbi <novadaya@gmail.com>
2024-03-25 16:44:39 +0100

    Waytracker init

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Test Page for PHP and HTML</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>  
+        <main class="main">
+            <div class="box">
+                <div class="container">
+                <div class="form">
+                <h1 class="login-h1">Login Form</h1>
+                    <div class="input-box">
+                        <input type="text" placeholder="Username">
+                    </div>
+                    <div class="input-box">
+                        <input type="password" placeholder="Password">
+                    </div>
+                    <div class="input-box">
+                        <button type="submit" value="Login">Login</button>
+                    </div>
+                    <p class="forgot">Forgot Password? <a href="#">Click Here</a></p>
+                    <p class="forgot">Don't have an account? <a href="#">Sign Up</a></p>
+                </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
