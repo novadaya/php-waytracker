a2901ee683bed813e3bde457f5d6d80eef51e103
Norbi <novadaya@gmail.com>
2024-04-11 15:28:37 +0200

    sing-in reg form design

--- myscript.js
+++ myscript.js
@@ -1,7 +1,14 @@
-var cards = document.querySelectorAll('.card');
+/* var cards = document.querySelectorAll('.card');
 
 [...cards].forEach((card)=>{
   card.addEventListener( 'click', function() {
     card.classList.toggle('is-flipped');
   });
-});
+}); */
+
+function myFunc(){
+  var cards = document.querySelectorAll('.card');
+  [...cards].forEach((card)=>{
+  card.classList.toggle('is-flipped');
+  });
+}
